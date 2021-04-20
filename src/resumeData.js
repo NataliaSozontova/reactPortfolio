36150c88202c9aab547cdfa47d758c81b8b5a1f5
Natalia <nasozontova@gmail.com>
2021-04-19 20:42:44 -0400

    adding react components

---
+++ src/resumeData.js
@@ -0,0 +1,127 @@
+let resumeData = {
+    "imagebaseurl":"https://rbhatia46.github.io/", //"https://github.com/NataliaSozontova"
+    "name": "Natalia Sozontova",
+    "role": "Software Developer Engineer in Test, Front End Developer, Back End Developer",
+    "linkedinId":"Your linkedinId",
+    // "skypeid": "Your skypeid",
+    "roleDescription": "I am currently working as an SDET,and I am writing the automation test scripts for various Web applications. I also completed full stack development bootcamp and like to develop different applications.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/rahul-bhatia-67ba08121/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/NataliaSozontova",
+          "className":"fa fa-github"
+        },
+        // {
+        //   "name":"skype",
+        //   "url":"http://twitter.com/rbhatia46",
+        //   "className":"fa fa-twitter"
+        // }
+      ],
+    "aboutme":"I am currently working as an SDET,and I am writing the automation test scripts for various Web applications. I also completed full stack development bootcamp and like to develop different applications.",
+    "address":"Virginia, USA",
+    "website":"https://github.com/NataliaSozontova",
+    "education":[
+      {
+        "UniversityName":"Baltic Federal University named after Imannuel Kant",
+        "specialization":"MS? Telecommunications",
+        "MonthOfPassing":"June",
+        "YearOfPassing":"2012",
+        "Achievements":"???Some Achievements"
+      },
+      {
+        "UniversityName":"Baltic Federal University named after Imannuel Kant",
+        "specialization":"BH",
+        "MonthOfPassing":"June",
+        "YearOfPassing":"2010",
+        "Achievements":"??Some Achievements"
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"OneGlobe LLC",
+        "specialization":"Software Developer Engineer in Test",
+        "MonthOfLeaving":"December",
+        "YearOfLeaving":"2019",
+        "Achievements":"??Some Achievements"
+      },
+      {
+        "CompanyName":"Some Company",
+        "specialization":"Some specialization",
+        "MonthOfLeaving":"Jan",
+        "YearOfLeaving":"2018",
+        "Achievements":"Some Achievements"
+      }
+    ],
+    "skillsDescription":"Your skills here",
+    "skills":[
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS3"
+      },
+      {
+        "skillname":"ReactJS"
+      },
+      {
+        "skillname":"JavaScript"
+      },
+      {
+        "skillname":"VanillaJS"
+      },
+      {
+        "skillname":"NodeJS"
+      },
+      {
+        "skillname":"MongoDB"
+      },
+      {
+        "skillname":"Mongoose"
+      },
+      {
+        "skillname":"PostgresSQL"
+      },
+      {
+        "skillname":"??Sequilise"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"project1",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      },
+      {
+        "name":"project2",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/project.jpg"
+      },
+      {
+        "name":"project3",
+        "description":"mobileapp",  
+        "imgurl":"images/portfolio/project2.png"
+      },
+      {
+        "name":"project4",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      }
+    ],
+    "testimonials":[
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      },
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      }
+    ]
+  }
+  
+  export default resumeData

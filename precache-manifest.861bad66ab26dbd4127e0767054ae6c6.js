8a9efb41db4891bb9c02598cc46f84374fc7b6b0
Natalia <nasozontova@gmail.com>
2021-04-25 13:46:44 -0400

    Updates

---
+++ precache-manifest.861bad66ab26dbd4127e0767054ae6c6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d29a0d14793120939721",
+    "url": "/reactPortfolio/static/css/main.abc1a30f.chunk.css"
+  },
+  {
+    "revision": "d29a0d14793120939721",
+    "url": "/reactPortfolio/static/js/main.d29a0d14.chunk.js"
+  },
+  {
+    "revision": "1048638a4991ffad96d3",
+    "url": "/reactPortfolio/static/js/1.1048638a.chunk.js"
+  },
+  {
+    "revision": "b0bfce8dd94b52aef958",
+    "url": "/reactPortfolio/static/js/runtime~main.b0bfce8d.js"
+  },
+  {
+    "revision": "2d6952336fb65c25008320a7133fe6a2",
+    "url": "/reactPortfolio/index.html"
+  }
+];
